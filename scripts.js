a8ada45dbc036335735f1143aa75990c24ef28fe
stratocastero <omarolmedoferrer@gmail.com>
2020-10-27 14:44:00 +0100

    pages added, content written, links fixed, pdf added

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 if (String(location.href).includes(".cat")){
-    // loads catalan content
+    // loads catalan content TO DO
 
 } else if (String(location.href).includes("/en")){
-    // loads english content
+    // loads english content TO DO
     
 }
 
